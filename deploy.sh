01e6c97d0f63daba4cb2bfbfe17316bb54182f5a
Clive Makamara <cmosh@live.com>
2017-09-22 20:04:17 +0300

    use correct path for now binary

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 set -e
 
 cd now
-now rm ics4 -y --token $NOW_TOKEN
-now --debug --public --force --token  $NOW_TOKEN
-now ls --token $NOW_TOKEN
-now alias  --token $NOW_TOKEN
-now scale ics4-2018.now.sh 1 --token $NOW_TOKEN
+./now-linux rm ics4 -y --token $NOW_TOKEN
+./now-linux --debug --public --force --token  $NOW_TOKEN
+./now-linux ls --token $NOW_TOKEN
+./now-linux alias  --token $NOW_TOKEN
+./now-linux scale ics4-2018.now.sh 1 --token $NOW_TOKEN
