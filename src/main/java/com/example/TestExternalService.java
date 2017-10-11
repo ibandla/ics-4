9206b78ec281c8257a07fcd9dad950bfb7ec830b
Clive Makamara <cmosh@live.com>
2017-10-11 12:35:42 +0300

    final

--- src/main/java/com/example/TestExternalService.java
+++ src/main/java/com/example/TestExternalService.java
@@ -1,32 +1,40 @@
 package com.example;
 
 import com.example.domain.Student;
+import com.example.models.Me;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by Elphas Khajira on 9/13/17.
- */
-//@Component
+
+@Component
 public class TestExternalService implements CommandLineRunner{
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
+    @Autowired
+    private final StudentFeignClient myfeignClient;
 
-    private final ExternalService externalService;
+    public TestExternalService(StudentFeignClient myfeignClient) {
+        this.myfeignClient = myfeignClient;
 
-    public TestExternalService(ExternalService externalService) {
-        this.externalService = externalService;
     }
 
     @Override
     public void run(String... strings) throws Exception {
-        List<Student> students=externalService.findAllStudentsFromExternalService();
-        students.forEach(student -> {
-            logger.info("Student name is "+student.getFirstName());
-        });
+//        Me first = new Me();
+//        first.setFullName("Clive Makamara");
+//        first.setadmissionNumber(82816);
+//        Me second = myfeignClient.createMe(first);
+//        List<Double> l = new ArrayList<>();
+//        l.add(121.1);
+//        l.add(122.4);
+//        Double answer = myfeignClient.calculate(l,second.getId().toString(),"Multiply");
+//        myfeignClient.validate(l,second.getId().toString(),answer);
+//        logger.info("Done");
     }
 }
